287707b34b27a523a857043df1109cb1e62ebf3f
nurmarief <ariefmuhammad897@gmail.com>
2024-02-15 18:37:19 +0700

    Update Feed component styling

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,13 +4,13 @@ import Posts from './Posts'
 
 export default function Feed() {
   return (
-    <main>
-        <section>
+    <main className='mx-auto md:max-w-6xl grid grid-cols-1 md:grid-cols-3'>
+        <section className='md:col-span-2'>
             <Stories/>
             <Posts />
         </section>
 
-        <section>
+        <section className='hidden md:inline-grid md:col-span-1'>
             {/* Mini Profile */}
 
             {/* Suggestions */}
